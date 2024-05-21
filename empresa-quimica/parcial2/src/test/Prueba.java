1d2fb413cb865864bd69a4d8ead0b8656489437a
Nicodan122 <nicolas.dangelo@davinci.edu.ar>
2024-05-21 15:44:05 -0300

    Subiendo Clase Test

---
+++ empresa-quimica/parcial2/src/test/Prueba.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Prueba {
+	
+	@Test
+	public void Pruebaparcial() {
+		assertEquals(true,true);
+		
+		
+	}
+
+}
